37355510745548911be303141aa349ed91dec89f
jasincere <772763242@qq.com>
2021-08-16 11:09:34 +0800

    feat(properties): add useProperties api

---
+++ src/constant.ts
@@ -0,0 +1,9 @@
+export declare const APP_CODE: string;
+
+export declare const API_DOMAIN: string;
+
+export declare const APP_VERSION: string;
+
+export declare const RUNTIME_ENV: string;
+
+export declare const BI_DOMAIN: string;
