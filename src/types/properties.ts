72ca562229f92708ac2b7e72508f15dd98ac97b4
jasincere <772763242@qq.com>
2021-08-21 16:32:46 +0800

    feat(useDataSource): DataSource subscriber

--- src/types/properties.ts
+++ src/types/properties.ts
@@ -13,20 +13,25 @@ export interface DynamicDataSource {
   fields: Field[];
 }
 
+export type DataSourceFetcher = <T = DataSourceResponse>(requestParams?: DataSourceRequestParams) => Promise<T>;
+
 export type DataSourceResult = {
   entities: Entity[];
   fields: Field[];
   selectedEntity: Entity;
   selectedEntityCode: string;
-  fetch<T = DataSourceResponse>(requestParams?: DataSourceRequestParams): Promise<T>;
+  fetchData: DataSourceFetcher;
 };
 
-export type UseDataSourceApi = (dataSource: DynamicDataSource) => DataSourceResult;
+export type DataSourceSubscriber = (fetchData: DataSourceFetcher) => void;
+
+export type UseDataSourceApi = (dataSource: DynamicDataSource, subscriber?: DataSourceSubscriber) => DataSourceResult;
 
 export type ObjectVariableType = 'object' | 'objectList' | 'jsonSchema';
 
 export type UsePropertiesApi<P = any> = () => {
   properties: P;
   setProperties(props: P): void;
+  setPropertyByKey<K extends keyof P>(key: K, propValue: P[K]): void;
   entities: Entity[];
 };
